e65e136632a4e56c079bb96830571159193143fc
AidinZaeim <aidinzaeim@gmail.com>
2023-11-26 17:55:55 +0330

    RBT debug

--- redbladktree.c
+++ redbladktree.c
@@ -2,281 +2,281 @@
 #include <stdlib.h>
 #include "proc.h"
 
-// Structure to represent each node in a red-black tree
-struct node {
-    struct proc* data; // process
-    int c;             // 1-red, 0-black
-    struct node* p;    // parent
-    struct node* r;    // right-child
-    struct node* l;    // left child
-};
+typedef enum { RED, BLACK } Color;
 
-struct node* root = NULL; // global root for the entire tree
+typedef struct Node {
+    struct proc* data;
+    Color color;
+    struct Node* parent;
+    struct Node* left;
+    struct Node* right;
+} Node;
 
-// Function to perform BST insertion of a node
-struct node* bst(struct node* trav, struct node* temp)
-{
-    // If the tree is empty, return a new node
-    if (trav == NULL)
-        return temp;
+typedef struct RBT {
+    Node* root;
+} RBT;
 
-    // Otherwise recur down the tree
-    if (temp->data->virtualtime < trav->data->virtualtime) {
-        trav->l = bst(trav->l, temp);
-        trav->l->p = trav;
-    }
-    else if (temp->data->virtualtime > trav->data->virtualtime) {
-        trav->r = bst(trav->r, temp);
-        trav->r->p = trav;
-    }
-
-    // Return the (unchanged) node pointer
-    return trav;
+Node* createNode(struct proc* data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->color = RED;
+    newNode->parent = NULL;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
 }
 
-// Function performing right rotation of the passed node
-void rightrotate(struct node* temp)
-{
-    struct node* left = temp->l;
-    temp->l = left->r;
-    if (temp->l)
-        temp->l->p = temp;
-    left->p = temp->p;
-    if (!temp->p)
-        root = left;
-    else if (temp == temp->p->l)
-        temp->p->l = left;
+RBT* createRBT() {
+    RBT* tree = (RBT*)malloc(sizeof(RBT));
+    tree->root = NULL;
+    return tree;
+}
+
+void leftRotate(RBT* tree, Node* x) {
+    Node* y = x->right;
+    x->right = y->left;
+    if (y->left != NULL)
+        y->left->parent = x;
+    y->parent = x->parent;
+
+    if (x->parent == NULL)
+        tree->root = y;
+    else if (x == x->parent->left)
+        x->parent->left = y;
     else
-        temp->p->r = left;
-    left->r = temp;
-    temp->p = left;
+        x->parent->right = y;
+
+    y->left = x;
+    x->parent = y;
 }
 
-// Function performing left rotation of the passed node
-void leftrotate(struct node* temp)
-{
-    struct node* right = temp->r;
-    temp->r = right->l;
-    if (temp->r)
-        temp->r->p = temp;
-    right->p = temp->p;
-    if (!temp->p)
-        root = right;
-    else if (temp == temp->p->l)
-        temp->p->l = right;
+void rightRotate(RBT* tree, Node* x) {
+    Node* y = x->left;
+    x->left = y->right;
+    if (y->right != NULL)
+        y->right->parent = x;
+    y->parent = x->parent;
+
+    if (x->parent == NULL)
+        tree->root = y;
+    else if (x == x->parent->left)
+        x->parent->left = y;
     else
-        temp->p->r = right;
-    right->l = temp;
-    temp->p = right;
+        x->parent->right = y;
+
+    y->right = x;
+    x->parent = y;
 }
 
-// This function fixes violations caused by BST insertion
-void fixup(struct node* root, struct node* pt)
-{
-    struct node* parent_pt = NULL;
-    struct node* grand_parent_pt = NULL;
-
-    while ((pt != root) && (pt->c != 0) && (pt->p->c == 1)) {
-        parent_pt = pt->p;
-        grand_parent_pt = pt->p->p;
-
-        if (parent_pt == grand_parent_pt->l) {
-            struct node* uncle_pt = grand_parent_pt->r;
-
-            if (uncle_pt != NULL && uncle_pt->c == 1) {
-                grand_parent_pt->c = 1;
-                parent_pt->c = 0;
-                uncle_pt->c = 0;
-                pt = grand_parent_pt;
-            }
-            else {
-                if (pt == parent_pt->r) {
-                    leftrotate(parent_pt);
-                    pt = parent_pt;
-                    parent_pt = pt->p;
+void fixInsertion(RBT* tree, Node* z) {
+    while (z->parent != NULL && z->parent->color == RED) {
+        if (z->parent == z->parent->parent->left) {
+            Node* y = z->parent->parent->right;
+            if (y != NULL && y->color == RED) {
+                z->parent->color = BLACK;
+                y->color = BLACK;
+                z->parent->parent->color = RED;
+                z = z->parent->parent;
+            } else {
+                if (z == z->parent->right) {
+                    z = z->parent;
+                    leftRotate(tree, z);
                 }
 
-                rightrotate(grand_parent_pt);
-                int t = parent_pt->c;
-                parent_pt->c = grand_parent_pt->c;
-                grand_parent_pt->c = t;
-                pt = parent_pt;
+                z->parent->color = BLACK;
+                z->parent->parent->color = RED;
+                rightRotate(tree, z->parent->parent);
+            }
+        } else {
+            Node* y = z->parent->parent->left;
+            if (y != NULL && y->color == RED) {
+                z->parent->color = BLACK;
+                y->color = BLACK;
+                z->parent->parent->color = RED;
+                z = z->parent->parent;
+            } else {
+                if (z == z->parent->left) {
+                    z = z->parent;
+                    rightRotate(tree, z);
+                }
+
+                z->parent->color = BLACK;
+                z->parent->parent->color = RED;
+                leftRotate(tree, z->parent->parent);
             }
         }
-        else {
-            struct node* uncle_pt = grand_parent_pt->l;
-
-            if ((uncle_pt != NULL) && (uncle_pt->c == 1)) {
-                grand_parent_pt->c = 1;
-                parent_pt->c = 0;
-                uncle_pt->c = 0;
-                pt = grand_parent_pt;
-            }
-            else {
-                if (pt == parent_pt->l) {
-                    rightrotate(parent_pt);
-                    pt = parent_pt;
-                    parent_pt = pt->p;
-                }
-
-                leftrotate(grand_parent_pt);
-                int t = parent_pt->c;
-                parent_pt->c = grand_parent_pt->c;
-                grand_parent_pt->c = t;
-                pt = parent_pt;
-            }
-        }        
     }
+
+    tree->root->color = BLACK;
 }
 
-// Function to find the minimum node in a given tree
-struct node* minValueNode(struct node* node)
-{
-    struct node* current = node;
+void insertRBT(RBT* tree, struct proc* data) {
+    Node* z = createNode(data);
+    Node* y = NULL;
+    Node* x = tree->root;
 
-    // Find the leftmost leaf node
-    while (current && current->l != NULL)
-        current = current->l;
+    while (x != NULL) {
+        y = x;
+        if (z->data->virtualtime < x->data->virtualtime)
+            x = x->left;
+        else
+            x = x->right;
+    }
 
-    return current;
-}
-
-// Function to replace a node with the child node
-void replaceNode(struct node* node, struct node* child)
-{
-    if (node->p == NULL)
-        root = child;
-    else if (node == node->p->l)
-        node->p->l = child;
+    z->parent = y;
+    if (y == NULL)
+        tree->root = z;
+    else if (z->data->virtualtime < y->data->virtualtime)
+        y->left = z;
     else
-        node->p->r = child;
+        y->right = z;
 
-    if (child != NULL)
-        child->p = node->p;
+    fixInsertion(tree, z);
 }
 
-// Function to fix the violations caused by BST deletion
-void fixDelete(struct node* node)
-{
-    struct node* sibling;
-    while (node != root && node->c == 0) {
-        if (node == node->p->l) {
-            sibling = node->p->r;
-
-            if (sibling->c == 1) {
-                sibling->c = 0;
-                node->p->c = 1;
-                leftrotate(node->p);
-                sibling = node->p->r;
+void fixDeletion(RBT* tree, Node* x) {
+    while (x != NULL && x != tree->root && x->color == BLACK) {
+        if (x == x->parent->left) {
+            Node* w = x->parent->right;
+            if (w != NULL && w->color == RED) {
+                w->color = BLACK;
+                x->parent->color = RED;
+                leftRotate(tree, x->parent);
+                w = x->parent->right;
             }
 
-            if (sibling->l->c == 0 && sibling->r->c == 0) {
-                sibling->c = 1;
-                node = node->p;
-            }
-            else {
-                if (sibling->r->c == 0) {
-                    sibling->l->c = 0;
-                    sibling->c = 1;
-                    rightrotate(sibling);
-                    sibling = node->p->r;
+            if ((w != NULL && w->left != NULL && w->left->color == BLACK) &&
+                (w != NULL && w->right != NULL && w->right->color == BLACK)) {
+                if (w != NULL)
+                    w->color = RED;
+                x = x->parent;
+            } else {
+                if (w != NULL && w->right != NULL && w->right->color == BLACK) {
+                    if (w->left != NULL)
+                        w->left->color = BLACK;
+                    if (w != NULL)
+                        w->color = RED;
+                    rightRotate(tree, w);
+                    w = x->parent->right;
                 }
 
-                sibling->c = node->p->c;
-                node->p->c = 0;
-                sibling->r->c = 0;
-                leftrotate(node->p);
-                node = root;
+                if (w != NULL)
+                    w->color = x->parent->color;
+                x->parent->color = BLACK;
+                if (w != NULL && w->right != NULL)
+                    w->right->color = BLACK;
+                leftRotate(tree, x->parent);
+                x = tree->root;
             }
-        }
-        else {
-            sibling = node->p->l;
+        } else {
+            Node* w = x->parent->left;
+            if (w != NULL && w->color == RED) {
+                w->color = BLACK;
+                x->parent->color = RED;
+                rightRotate(tree, x->parent);
+                w = x->parent->left;
+            }
 
-            if (sibling->c == 1) {
-                sibling->c = 0;
-                node->p->c = 1;
-                rightrotate(node->p);
-                sibling = node->p->l;
-            }
-
-            if (sibling->r->c == 0 && sibling->l->c == 0) {
-                sibling->c = 1;
-                node = node->p;
-            }
-            else {
-                if (sibling->l->c == 0) {
-                    sibling->r->c = 0;
-                    sibling->c = 1;
-                    leftrotate(sibling);
-                    sibling = node->p->l;
+            if ((w != NULL && w->right != NULL && w->right->color == BLACK) &&
+                (w != NULL && w->left != NULL && w->left->color == BLACK)) {
+                if (w != NULL)
+                    w->color = RED;
+                x = x->parent;
+            } else {
+                if (w != NULL && w->left != NULL && w->left->color == BLACK) {
+                    if (w->right != NULL)
+                        w->right->color = BLACK;
+                    if (w != NULL)
+                        w->color = RED;
+                    leftRotate(tree, w);
+                    w = x->parent->left;
                 }
 
-                sibling->c = node->p->c;
-                node->p->c = 0;
-                sibling->l->c = 0;
-                rightrotate(node->p);
-                node = root;
+                if (w != NULL)
+                    w->color = x->parent->color;
+                x->parent->color = BLACK;
+                if (w != NULL && w->left != NULL)
+                    w->left->color = BLACK;
+                rightRotate(tree, x->parent);
+                x = tree->root;
             }
         }
     }
-    node->c = 0;
+
+    if (x != NULL)
+        x->color = BLACK;
+}
+Node* checkPID(Node* node,int pid){
+    if(node == NULL){
+        return NULL;
+    }
+    else if(node->data->pid == pid){
+        return node;
+    }    
+    else{
+        Node* left = checkPID(node->left, pid);
+        Node* right = checkPID(node->right, pid);
+        if(right == NULL && left == NULL){
+            return NULL;
+        }
+        else if(right == NULL){
+            return left;
+        }
+        else{
+            return right;
+        }
+    }
+    
 }
 
-// Function to delete a node from the red-black tree
-struct node* deleteNode(struct node* root, int key)
-{
-    struct node* node = root;
-    struct node* parent = NULL;
-    struct node* toDelete = NULL;
-    int found = 0;
+void deleteRBT(RBT* tree, int pid) {
+    Node* z = tree->root;
+    Node* x;
+    Node* y;
 
-    while (node != NULL && found == 0) {
-        if (node->data->pid == key) {
-            found = 1;
-            toDelete = node;
-        }
+    z = checkPID(z, pid);
+    
+    if (z == NULL)
+        return;
 
-        if (found == 0) {
-            if (node->data->pid < key)
-                node = node->r;
-            else
-                node = node->l;
-        }
-    }
-
-    if (found == 0) {        
-        return root;
-    }
-
-    int color = toDelete->c;
-    struct node* child;
-    if (toDelete->l == NULL) {
-        child = toDelete->r;
-        replaceNode(toDelete, toDelete->r);
-    }
-    else if (toDelete->r == NULL) {
-        child = toDelete->l;
-        replaceNode(toDelete, toDelete->l);
-    }
+    if (z->left == NULL || z->right == NULL)
+        y = z;
     else {
-        struct node* minNode = minValueNode(toDelete->r);
-        color = minNode->c;
-        child = minNode->r;
-        if (minNode->p == toDelete)
-            parent = minNode;
-        else {
-            replaceNode(minNode, minNode->r);
-            minNode->r = toDelete->r;
-            minNode->r->p = minNode;
-        }
-        replaceNode(toDelete, minNode);
-        minNode->l = toDelete->l;
-        minNode->l->p = minNode;
-        minNode->c = toDelete->c;
+        Node* temp = z->right;
+        while (temp->left != NULL)
+            temp = temp->left;
+        y = temp;
     }
 
-    if (color == 0)
-        fixDelete(child);
+    if (y->left != NULL)
+        x = y->left;
+    else
+        x = y->right;
 
-    return root;
+    if (x != NULL)
+        x->parent = y->parent;
+
+    if (y->parent == NULL)
+        tree->root = x;
+    else if (y == y->parent->left)
+        y->parent->left = x;
+    else
+        y->parent->right = x;
+
+    if (y != z)
+        z->data = y->data;
+
+    if (y->color == BLACK)
+        fixDeletion(tree, x);
+
+    free(y);
+}
+
+void printRBTInOrder(Node* node) {
+    if (node == NULL)
+        return;
+
+    printRBTInOrder(node->left);
+    printf("%d ", node->data);
+    printRBTInOrder(node->right);
 }
